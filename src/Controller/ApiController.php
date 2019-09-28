2c46096cca70b0e9914bd92541e9f5f7877fb885
sfreire <sfreire@chequemotiva.com>
2019-09-28 19:46:41 +0200

    New Feature: api endpoint for get detailed information of the recipes plus several refactorizations

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,10 +4,14 @@
 namespace App\Controller;
 
 
+use App\Exception\ApiInvalidRequestException;
 use App\Services\GuzzleClientService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class ApiController extends AbstractFOSRestController
+abstract class ApiController extends AbstractFOSRestController
 {
     const DEFAULT_NULL = null;
 
@@ -17,5 +21,38 @@ class ApiController extends AbstractFOSRestController
     {
         $this->guzzleClientService = $guzzleClientService;
     }
+
+    protected function makePuppyRecipesRequest(Request $request)
+    {
+        try {
+            $options = $this->prepareOptions($request);
+
+            $statusCode = Response::HTTP_OK;
+            $puppyResponse = $this->guzzleClientService->request(
+                Request::METHOD_GET,
+                'http://www.recipepuppy.com/api/',
+                $options
+            );
+            $response = array(
+                "results" => $this->prepareResponseData($puppyResponse)
+            );
+        } catch (ApiInvalidRequestException $argumentException) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $response = array(
+                'message' => $argumentException->getMessage()
+            );
+        } catch (\Exception $ex) {
+            $statusCode = Response::HTTP_FAILED_DEPENDENCY;
+            $response = array(
+                'message' => 'There was an error on the request!'
+            );
+        }
+
+        return new JsonResponse($response, $statusCode);
+    }
+
+    abstract protected function prepareOptions(Request $request);
+
+    abstract protected function prepareResponseData($data);
 
 }
